25da7291e804e58b354ec54c7747bdd6fd7d7080
zac <zdenekdoskocil88@gmail.com>
2024-06-07 12:04:02 +0200

    7/6/2024

---
+++ printf_ft/ft_numbers.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_numbers.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zac <zac@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 09:42:38 by zac               #+#    #+#             */
+/*   Updated: 2024/06/07 11:54:43 by zac              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_number(int number, int *length)
+{
+	if (number == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		(*length) += 11;
+		return ;
+	}
+	if (number < 0)
+	{
+		ft_putcharacter_length('-', length);
+		ft_number(number * -1, length);
+	}
+	else
+	{
+		if (number > 9)
+			ft_number(number / 10, length);
+		ft_putcharacter_length(number % 10 + '0', length);
+	}
+}
+
+void	ft_pointer(size_t pointer, int *length)
+{
+	char	string[25];
+	int		i;
+	char	*base_character;
+
+	base_character = "0123456789abcdef";
+	i = 0;
+	write(1, "0x", 2);
+	(*length) += 2;
+	if (pointer == 0)
+	{
+			ft_putcharacter_length('0', length);
+			return ;
+	}
+	while (pointer != 0)
+	{
+			string[i] = base_character[pointer % 16];
+	}
+}
+
+int main()
+{
+	int length = 0;
+	ft_number(-2147483648, &length);
+	return (0);
+}
---
+++ printf_ft/ft_words.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_words.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zac <zac@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 09:04:20 by zac               #+#    #+#             */
+/*   Updated: 2024/06/07 12:03:23 by zac              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+//  write a character to the standard output and increment the length counter
+
+void	ft_putcharacter_length(char character, int *length)
+{
+	write(1, &character, 1);
+		(*length)++;
+
+}
+
+// write a string to the standard output and update the length counter.
+
+void	ft_string(char *args, int *length)
+{
+	size_t	i;
+
+	i = 0;
+	if (!args)
+	{
+		write(1, "(null)", 6);
+		(*length) += 6;
+		return ;
+	}
+	while (args[i])
+	{
+		ft_putcharacter_length(args[i], length);
+		i++;
+	}
+}
+/*
+#include <stdio.h>
+#include <stddef.h>
+int main()
+{
+    int length = 0; // Initialize a length counter to 0.
+
+    // Demonstrate writing individual characters.
+    ft_putcharacter_length('H', &length);
+    ft_putcharacter_length('e', &length);
+    ft_putcharacter_length('l', &length);
+    ft_putcharacter_length('l', &length);
+    ft_putcharacter_length('o', &length);
+
+    // Write a space character.
+    ft_putcharacter_length(' ', &length);
+
+    // Demonstrate writing a string.
+    ft_string("world!", &length);
+
+    // Write a newline character.
+    ft_putcharacter_length('\n', &length);
+
+    // Demonstrate writing a null string.
+    ft_string(NULL, &length);
+
+    // Write a final newline character.
+    ft_putcharacter_length('\n', &length);
+
+    // Print the total length.
+    char buffer[50];
+    int len = snprintf(buffer, sizeof(buffer), "Total characters written: %d\n", length);
+    write(1, buffer, len);
+
+    return 0;
+}
+*/
