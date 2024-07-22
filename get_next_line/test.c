b633dcb122bd8088ad121f8610fdd77dedd356a8
zac <zdenekdoskocil88@gmail.com>
2024-07-22 11:32:12 +0200

    ...

--- get_next_line/test.c
+++ get_next_line/test.c
@@ -40,13 +40,33 @@
 
 // EXAMPLE 3)
 
-int main (){
-	int fd;
+// int main (){
+// 	int fd;
 
-	fd = open("test2.txt", O_RDWR | O_CREAT | O_APPEND);
+// 	fd = open("test2.txt", O_RDWR | O_CREAT | O_APPEND);
 
-	dup2(fd, 1);
+// 	dup2(fd, 1);
  
-	write(4, "hello22", 7);
+// 	write(4, "hello22", 7);
+// 	return (0);
+// }
+
+// EXAMPLE 4)
+
+struct Person {
+	char name[10];
+	int age;
+};
+
+void updateStruct(struct Person *p, int age)
+{
+	p->age = age;
+}
+
+int main(int argc, char **argv)
+{
+	struct Person lowlevellearning;
+	updateStruct(&lowlevellearning, 100);
+
 	return (0);
 }
--- get_next_line/testStruct.c
+++ get_next_line/testStruct.c
@@ -16,7 +16,7 @@ hovinko *hovno2 = malloc(sizeof(hovinko));
 hovinko *hovno3 = malloc(sizeof(hovinko));
 hovno1->cislo = 11;
 hovno2->cislo = 22;
- hovno3->cislo = 33;
+hovno3->cislo = 33;
 
  hovno1->dalsi = hovno2;
  hovno2->dalsi = hovno3;
