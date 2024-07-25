45263f92b3d789a0dec53d8dd6cd3c54fdd94d57
Zdenek Doskocil <zdenekdoskocil88@gmail.com>
2024-07-25 18:32:31 +0200

    ....

--- get_next_line/test.c
+++ get_next_line/test.c
@@ -1,72 +1,15 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
-// EXAMPLE 1)
+char *strchr(const char *s, int c);
 
-// int main (){
-// 	int fd;
 
-// 	fd = open("test.txt", O_RDONLY | O_CREAT);
-// 	printf("fd of text.txt: %d", fd);
-// 	return (0);
-// }
-
-// EXAMPLE 2)
-
-// int main (){
-// 	int fd;
-
-// 	fd = open("test2.txt", O_RDWR | O_CREAT);
-// 	printf("fd of test2.txt: %d\n", fd);
-// 	write(1, "hello", 5);
-// 	//write(3, "hello22", 7);
-// 	write(fd, "hello22", 7);
-// 	return (0);
-// }
-
-// EXAMPLE 3)
-
-// int main (){
-// 	int fd;
-
-// 	fd = open("test2.txt", O_RDWR | O_CREAT | O_APPEND);
-
-// 	dup2(fd, 1);
- 
-// 	write(4, "hello22", 7);
-// 	return (0);
-// }
-
-// EXAMPLE 3)
-
-// int main (){
-// 	int fd;
-
-// 	fd = open("test2.txt", O_RDWR | O_CREAT | O_APPEND);
-
-// 	dup2(fd, 1);
- 
-// 	write(4, "hello22", 7);
-// 	return (0);
-// }
-
-// EXAMPLE 4)
-
-struct Person {
-	char name[10];
-	int age;
-};
-
-void updateStruct(struct Person *p, int age)
+int main()
 {
-	p->age = age;
-}
-
-int main(int argc, char **argv)
-{
-	struct Person lowlevellearning;
-	updateStruct(&lowlevellearning, 100);
-
-	return (0);
+	char *s = "test";
+	int	c = 101;
+	printf("%p",strchr(s, c));
+	return 0;
 }
--- get_next_line/testfile.c
+++ get_next_line/testfile.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   testfile.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zac <zac@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: zdoskoci <zdoskoci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 11:18:10 by zdoskoci          #+#    #+#             */
-/*   Updated: 2024/07/25 00:04:59 by zac              ###   ########.fr       */
+/*   Updated: 2024/07/25 18:29:37 by zdoskoci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,19 +41,19 @@ size_t	ft_strlen(const char *s)
 }
 
 
-char	*ft_strcpy(char *dest, const char *src)
-{
-	int	i;
+// char	*ft_strcpy(char *dest, const char *src)
+// {
+// 	int	i;
 
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
+// 	i = 0;
+// 	while (src[i] != '\0')
+// 	{
+// 		dest[i] = src[i];
+// 		i++;
+// 	}
+// 	dest[i] = '\0';
+// 	return (dest);
+// }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -77,6 +77,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		j++;
 	}
 	str[i + j] = '\0';
+
 	return (str);
 }
 
@@ -86,30 +87,28 @@ int main()
 	char	*buf; //get rid of 100 and malloc instead
 	int		chars_read;
 	int		bufSize;
-	char	*storage;
-	char	testString[50];	
 	char	*finalString;
-	char	test1[]="hello";
-	char	test2[]="world";
-	char	test3[]="again";
+	char	*temp;
 
-		// finalString = ft_strjoin(test1,test2);
-		// printf("%s", finalString);
-		// finalString = ft_strjoin(finalString,test2);
-		// printf("%s", finalString);
-	
-	bufSize = 12;
+	bufSize = 15;
 	buf  = malloc(sizeof(char) * (bufSize + 1));
 	fd = open("test2.txt", O_RDONLY);
+	//fd = 1;
 	finalString = malloc(sizeof(char) * 50);
 	//while((chars_read = read(fd, buf, bufSize)))
 	while((chars_read = read(fd, buf, bufSize)))
 	{
 		finalString = ft_strjoin(finalString, buf);
-		//ft_strcpy(testString, buf);
-		printf("%s\n", finalString);
-		ft_bzero(buf,13);
+		temp = finalString;
+		free(finalString);
+		printf("%s\n", buf);
+		printf("%p\n", temp);
+		ft_bzero(buf,16);
 	}
+		printf("finalString: %p\n", temp);
+	free(buf);
+
+
 	return (0);
 }
-//valgrind --leak-check=full ./a.out
+//valgrind --leak-check=full ./a.out 
