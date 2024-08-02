75ea7062bc1cb4c18447ffc0c5f0a154c4ebcfbf
Zdenek Doskocil <zdenekdoskocil88@gmail.com>
2024-08-02 19:17:06 +0200

    ...

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: zdoskoci <zdoskoci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 18:55:09 by zac               #+#    #+#             */
-/*   Updated: 2024/08/02 19:06:43 by zdoskoci         ###   ########.fr       */
+/*   Updated: 2024/08/02 19:16:16 by zdoskoci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ char	*get_next_line(int fd)
 	line = ft_clear_line(buffer);
 	buffer = ft_rest_start(buffer);
 	//printf("gnl - BUF AFTER REST_START: --%s--\n", buffer);
-
 
 	return (line);
 }
