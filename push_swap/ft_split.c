107f2169423ba64fbb44424d2173163e67fd6c0c
zac <zdenekdoskocil88@gmail.com>
2024-12-29 21:58:40 +0100

    split

--- push_swap/ft_split.c
+++ push_swap/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: zac <zac@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 20:33:06 by zac               #+#    #+#             */
-/*   Updated: 2024/12/25 11:04:58 by zac              ###   ########.fr       */
+/*   Updated: 2024/12/29 21:58:13 by zac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ static	void	ft_freeup(char *strs)
 	free(strs);
 }
 
-static int ft_wordcount(char *str, char c)
+static int	ft_wordcounter(char *str, char c)
 {
 	int	i;
-	int word;
+	int	word;
 
 	i = 0;
 	word = 0;
@@ -45,4 +45,69 @@ static int ft_wordcount(char *str, char c)
 		i++;
 	}
 	return (word);
+}
+
+static void	ft_strcpy(char *word, char *str, char c, int j)
+{
+	int	i;
+
+	i = 0;
+	while (str[j] != '\0' && str[j] == c)
+		j++;
+	while (str[j + i] != c && str[j + i] != '\0')
+	{
+		word[i] = str[j + i];
+		i++;
+	}
+	word[i] = '\0';
+}
+
+static char	*ft_stralloc(char *str, char c, int *k)
+{
+	char	*word;
+	int		j;
+
+	j = *k;
+	word = (NULL);
+	while (str[*k] != '\0')
+	{
+		if (str[*k] != c)
+		{
+			while (str[*k] != '\0' && str[*k] != c)
+				*k += 1;
+			word = (char *)malloc(sizeof(char) * (*k + 1));
+			if (word == NULL)
+				return (NULL);
+			break ;
+		}
+		*k += 1;
+	}
+	ft_strcpy(word, str, c, j);
+	return (word);
+}
+
+char	**ft_split(char const *str, char c)
+{
+	char	**strs;
+	int		i;
+	int		j;
+	int		position;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	position = 0;
+	j = ft_wordcounter((char *)str, c);
+	strs = (char **)malloc(sizeof(char *) * (j + 1));
+	if (strs == NULL)
+		return (NULL);
+	strs[j] = NULL;
+	while (i < j)
+	{
+		strs[i] = ft_stralloc(((char *)str), c, &position);
+		if (strs[i] == NULL)
+			ft_freeup(strs[i]);
+		i++;
+	}
+	return (strs);
 }
